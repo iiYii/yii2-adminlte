21a64b5c25e0493df2cd9b37f8b5949636eacb3d
forecho <caizhenghai@gmail.com>
2016-05-24 16:03:52 +0800

    优化语言包；日志；搜索Model

--- common/config/main.php
+++ common/config/main.php
@@ -8,35 +8,112 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'authManager' => [
-            'class' => 'yii\rbac\DbManager',
-        ],
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            //'enableStrictParsing' => true,
-            'rules' => [
-            ],
+        ],
+        'formatter' => [
+            'class' => 'yii\i18n\Formatter',
+            'dateFormat' => 'php:d-M-Y',
+            'datetimeFormat' => 'php:d-M-Y H:i:s',
+            'timeFormat' => 'php:H:i:s',
         ],
         'i18n' => [
             'translations' => [
-                '*' => [
+                'app*' => [
                     'class' => 'yii\i18n\PhpMessageSource',
-                    //'basePath' => '@app/messages',
-                    //'sourceLanguage' => 'en',
-                    'fileMap' => [
-                        'app' => 'app.php',
-                        'app/error' => 'error.php',
-                    ],
+                    'basePath' => '@common/messages',
                 ],
             ],
         ],
-        'formatter' => [
-            'dateFormat' => 'yyyy-MM-dd',
-            'datetimeFormat' => 'yyyy-MM-dd HH:mm:ss',
-            'decimalSeparator' => ',',
-            'thousandSeparator' => ' ',
-            'currencyCode' => 'CNY',
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                /**
+                 * 错误级别日志：当某些需要立马解决的致命问题发生的时候，调用此方法记录相关信息。
+                 * 使用方法：Yii::error()
+                 */
+                [
+                    'class' => 'common\components\FileTarget',
+                    // 日志等级
+                    'levels' => ['error'],
+                    // 被收集记录的额外数据
+                    'logVars' => ['_GET', '_POST', '_FILES', '_COOKIE', '_SESSION', '_SERVER'],
+                    // 指定日志保存的文件名
+                    'logFile' => '@app/runtime/logs/error/app.log',
+                    // 是否开启日志 (@app/runtime/logs/error/20151223_app.log)
+                    'enableDatePrefix' => true,
+                ],
+                /**
+                 * 警告级别日志：当某些期望之外的事情发生的时候，使用该方法。
+                 * 使用方法：Yii::warning()
+                 */
+                [
+                    'class' => 'common\components\FileTarget',
+                    // 日志等级
+                    'levels' => ['warning'],
+                    // 被收集记录的额外数据
+                    'logVars' => ['_GET', '_POST', '_FILES', '_COOKIE', '_SESSION', '_SERVER'],
+                    // 指定日志保存的文件名
+                    'logFile' => '@app/runtime/logs/warning/app.log',
+                    // 是否开启日志 (@app/runtime/logs/warning/20151223_app.log)
+                    'enableDatePrefix' => true,
+                ],
+                /**
+                 * info 级别日志：在某些位置记录一些比较有用的信息的时候使用。
+                 * 使用方法：Yii::info()
+                 */
+                [
+                    'class' => 'common\components\FileTarget',
+                    // 日志等级
+                    'levels' => ['info'],
+                    // 被收集记录的额外数据
+                    'logVars' => ['_GET', '_POST', '_FILES', '_COOKIE', '_SESSION', '_SERVER'],
+                    // 指定日志保存的文件名
+                    'logFile' => '@app/runtime/logs/info/app.log',
+                    // 是否开启日志 (@app/runtime/logs/info/20151223_app.log)
+                    'enableDatePrefix' => true,
+                ],
+                /**
+                 * trace 级别日志：记录关于某段代码运行的相关消息。主要是用于开发环境。
+                 * 使用方法：Yii::trace()
+                 */
+                [
+                    'class' => 'common\components\FileTarget',
+                    // 日志等级
+                    'levels' => ['trace'],
+                    // 被收集记录的额外数据
+                    'logVars' => ['_GET', '_POST', '_FILES', '_COOKIE', '_SESSION', '_SERVER'],
+                    // 指定日志保存的文件名
+                    'logFile' => '@app/runtime/logs/trace/app.log',
+                    // 是否开启日志 (@app/runtime/logs/trace/20151223_app.log)
+                    'enableDatePrefix' => true,
+                ],
+                [
+                    'class' => 'common\components\FileTarget',
+                    'levels' => ['info'],
+                    'logVars' => [], //除了except对应的分类之外，其他的都写入到
+                    'categories' => ['curl'],
+                    'logFile' => '@app/runtime/logs/curl/app.log',
+                    'enableDatePrefix' => true,
+                ],
+                [
+                    'class' => 'common\components\FileTarget',
+                    'levels' => ['info'],
+                    'categories' => ['request'],
+                    'logVars' => [], //除了except对应的分类之外，其他的都写入到
+                    'logFile' => '@app/runtime/logs/request/app.log',
+                    'enableDatePrefix' => true,
+                ],
+                [
+                    'class' => 'common\components\FileTarget',
+                    'levels' => ['error', 'warning', 'info', 'trace', 'profile'],
+                    'logVars' => [],
+                    'maxFileSize' => 1024,
+                    'logFile' => '@app/runtime/logs/app/app.log',
+                    'enableDatePrefix' => true,
+                ],
+            ],
         ],
     ],
 ];
---
+++ common/messages/zh-CN/app.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'DELETED' => '已删除',
+    'ACTIVE' => '正常',
+
+    'Login' => '登录',
+    'Signup' => '注册',
+    'Logout' => '退出',
+    'User Profile' => '用户设置',
+
+    'Username' => '用户名',
+//    'Username' => '用户名/邮箱',
+    'Email' => '邮箱',
+    'Password' => '密码',
+    'Remember Me' => '记住我',
+    'Invite Code' => '邀请码',
+    'Incorrect username or password.' => '用户名密码验证失败。',
+    'You don\'t have permission to login.' => '你没有登录权限。',
+    'User ID' => '用户 ID',
+    'Type' => '分类',
+    'Created At' => '创建时间',
+    'Updated At' => '更新时间',
+    'Description' => '描述',
+
+    'Forgot password?' => '忘记密码?',
+    'Save' => '保存',
+    'This username can only be numbers or letters' => '用户名只能为数字或者字母',
+    'This username has already been taken.' => '用户名已经存在',
+    'This email address has already been taken.' => '邮箱地址已经存在',
+    'This invite code is can\'t use' => '邀请码不正确',
+
+    'About' => '关于我们',
+    'Submit' => '提交',
+    'Contact' => '反馈',
+];
